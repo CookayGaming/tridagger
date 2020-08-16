164c4f7666c42a79277cfcb16d107b6a74e6061f
Cookay <58051848+CookayGaming@users.noreply.github.com>
2020-08-16 21:32:09 +0200

    Add files via upload

---
+++ tridagger/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+files {
+	'contentunlocks.meta',
+	'loadouts.meta',
+	'pedpersonality.meta',
+	'weaponanimations.meta',
+	'weaponarchetypes.meta',
+	'weapons.meta',
+}
+
+data_file 'WEAPONINFO_FILE' 'weapons.meta'
+data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'contentunlocks.meta'
+data_file 'LOADOUTS_FILE' 'loadouts.meta'
+data_file 'PED_PERSONALITY_FILE' 'pedpersonality.meta'
